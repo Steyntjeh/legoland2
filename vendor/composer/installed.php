dc2ae53ee90bf698f353ccbe5292f110d884ea21
Yannick <yannick.huet@student.graafschapcollege.nl>
2024-04-15 19:28:16 +0200

    css updated

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '16da0b9158c7bea2b7b8ed1ba3de83ddb460150a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '16da0b9158c7bea2b7b8ed1ba3de83ddb460150a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

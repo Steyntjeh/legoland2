42a3a769a52209bc616df40d766b46a630747ed4
Steyntjeh <steyn.ruesink@gmail.com>
2024-05-13 09:18:22 +0100

    Boekingen form gemaakt

--- resources/views/Boeken.blade.php
+++ resources/views/Boeken.blade.php
@@ -18,8 +18,32 @@
             <input type="tel" name="telefoonnummer" id="telefoonnummer" class="form-control" placeholder="Telefoonnummer...">
         </div>   
         <div class="form-group">
-            <label for="naam">Kamertype*</label>
-        </div> 
+            <label for="kamertype">Kamertype*</label>
+            <select>
+                <option>
+                    <select name="kamers" id="kamertype">
+                        <option value="">--Please choose an option--</option>
+                        <option value="vierPersonen">Huisje 4 personen</option>
+                        <option value="zesPersonen">Huisje 6 personen</option>
+                        <option value="tienPersonen">Huisje 10 personen</option>
+                        <option value="hotel">Hotel</option>
+                    </select>
+                </option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="aankomst">Aankomst datum*</label>
+            <input type="date" name="aankomst" id="aankomst" class="form-control" placeholder="dd/mm/yyyy">
+        </div>   
+        <div class="form-group">
+            <label for="vertrek">Vertrek datum*</label>
+            <input type="date" name="vertrek" id="vertrek" class="form-control" placeholder="dd/mm/yyyy">
+        </div>
+        <div class="form-group">
+            <label for="aantalPersonen">Aantal personen*</label>
+            <input type="number" name="aantalPersonen" id="aantalPersonen" min="1"
+                max="10" class="form-control" placeholder="0">
+        </div>
         <button type="submit" class="btn btn-primary">Boeken</button>
     </form>
 

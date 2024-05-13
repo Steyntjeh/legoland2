17086baf754e61aedf55ae9f8fb89182a8f98d8a
Yannick <yannick.huet@student.graafschapcollege.nl>
2024-05-13 09:08:51 +0200

    second commit

--- resources/views/Details.blade.php
+++ resources/views/Details.blade.php
@@ -7,6 +7,9 @@
                 <img src="{{$attractie['image']}}" alt="">
                 <h2>{{$attractie['naamAttractie']}}</h2>
                 <p>{{$attractie['informatie']}}</p>
+                <p>lengte attractie: {{$attractie['lengte']}}</p>
+                <p>tijdsduur attractie: {{$attractie['tijdsDuur']}}</p>
+                <p>Bekijk <a href="/attracties">hier</a> de andere attracties</p>
             </div>
         @else
             <p>Attractie niet gevonden.</p>

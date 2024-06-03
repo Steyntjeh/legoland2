96255d2cf535eefeb4e0f8a3521986bad9bc571f
Yannick <yannick.huet@student.graafschapcollege.nl>
2024-06-03 09:11:13 +0200

    register to admin

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,12 +22,12 @@
         <li><a href="/tickets">Tickets</a></li>
         <li><a href="/accomodatie">Accomodaties</a></li>
         <li><a href="/login">Login</a></li>
-        <li><a href="/register">Register</a></li>
     @endguest
     @auth
         <li><a href="/contact">Contacten</a></li>
         <li><a href="/contact">Boekingen</a></li>
         <li><a href="/dashboard">Dashboard</a></li>
+        <li><a href="/register">Register</a></li>
         <li><a href="/logout">Logout</a></li>
     @endauth
   </ul>

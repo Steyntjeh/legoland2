e87ef2f61f6e11096fa0e7ffff54ff317222c3ba
Yannick <yannick.huet@student.graafschapcollege.nl>
2024-06-03 09:19:11 +0200

    overzichten added

---
+++ app/Http/Controllers/BoekOverzichtController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\boekOverzicht;
+use Illuminate\Http\Request;
+
+class BoekOverzichtController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(boekOverzicht $boekOverzicht)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(boekOverzicht $boekOverzicht)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, boekOverzicht $boekOverzicht)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(boekOverzicht $boekOverzicht)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ContactOverzichtController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\contactOverzicht;
+use Illuminate\Http\Request;
+
+class ContactOverzichtController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(contactOverzicht $contactOverzicht)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(contactOverzicht $contactOverzicht)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, contactOverzicht $contactOverzicht)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(contactOverzicht $contactOverzicht)
+    {
+        //
+    }
+}
